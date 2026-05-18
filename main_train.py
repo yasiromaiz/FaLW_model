38dee2c622fd338f64cb10482c300c20b50e7cdc
yasiromaiz <yasiromaiz786@gmail.com>
2026-05-18 22:41:32 +0530

    add the import setup_model_dataset

--- main_train.py
+++ main_train.py
@@ -23,7 +23,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from train import train
 from val import validate
 from utils import *
-from utils import setup_model_dataset
+from utilities import setup_model_dataset
 # from utils import NormalizeByChannelMeanStd
 import sys 
 best_sa = 0
