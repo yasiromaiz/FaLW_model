90672738d400eef97bbdd41ee461e30ee2af7c69
yasiromaiz <yasiromaiz786@gmail.com>
2026-05-18 23:15:03 +0530

    adding missing training utilities

--- utils.py
+++ utils.py
@@ -327,8 +327,6 @@ def global_prune_model(
 # added this below code :-
 
 class AverageMeter:
-    """Computes and stores the average and current value"""
-
     def __init__(self):
         self.reset()
 
@@ -343,3 +341,23 @@ class AverageMeter:
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+
+        return res
+        
