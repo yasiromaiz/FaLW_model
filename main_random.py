af3c9b78384bb8abd9671b362d149a80b68988a1
yasiromaiz <yasiromaiz786@gmail.com>
2026-05-20 07:46:20 +0530

    Fixed validate import in main_random.py

--- main_random.py
+++ main_random.py
@@ -11,7 +11,8 @@ import torch.optim
 import torch.utils.data
 import unlearn_method as unlearn
 import utils
-from trainer import validate
+# from trainer import validate
+from val import validate
 import warnings
 warnings.filterwarnings("ignore")
 
