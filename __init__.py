8c2f9d825f5fa234462ce05f349c7c1255c23f51
yasiromaiz <yasiromaiz786@gmail.com>
2026-05-25 21:48:25 +0530

    Changed from relative import to actual running

--- __init__.py
+++ __init__.py
@@ -1,20 +1,48 @@
-from .GA import GA,GA_l1
-from .RL import RL
-from .FT import FT,FT_l1
-from .fisher import fisher,fisher_new
-from .retrain import retrain
-from .impl import load_unlearn_checkpoint, save_unlearn_checkpoint
-from .Wfisher import Wfisher
-from .FT_prune import FT_prune
-from .FT_prune_bi import FT_prune_bi
-from .GA_prune_bi import GA_prune_bi
-from .GA_prune import GA_prune
+# The below were correct for the [ FaLW, RL, BS, FT, GA models ] ; now for the BE it is showing error as 
+# [ we change the import to __intit__ in main_forget.py so ]
 
-from .RL_pro import RL_proximal
-from .boundary_ex import boundary_expanding
-from .boundary_sh import boundary_shrink
-from .FaLW import FaLW
-from .SFRon import SFRonExe
+
+
+# from .GA import GA,GA_l1
+# from .RL import RL
+# from .FT import FT,FT_l1
+# from .fisher import fisher,fisher_new
+# from .retrain import retrain
+# from .impl import load_unlearn_checkpoint, save_unlearn_checkpoint
+# from .Wfisher import Wfisher
+# from .FT_prune import FT_prune
+# from .FT_prune_bi import FT_prune_bi
+# from .GA_prune_bi import GA_prune_bi
+# from .GA_prune import GA_prune
+
+# from .RL_pro import RL_proximal
+# from .boundary_ex import boundary_expanding
+# from .boundary_sh import boundary_shrink
+# from .FaLW import FaLW
+# from .SFRon import SFRonExe
+
+
+
+
+# Updated to this below code 
+from GA import GA,GA_l1
+from RL import RL
+from FT import FT,FT_l1
+from fisher import fisher,fisher_new
+from retrain import retrain
+from impl import load_unlearn_checkpoint, save_unlearn_checkpoint
+from Wfisher import Wfisher
+from FT_prune import FT_prune
+from FT_prune_bi import FT_prune_bi
+from GA_prune_bi import GA_prune_bi
+from GA_prune import GA_prune
+
+from RL_pro import RL_proximal
+from boundary_ex import boundary_expanding
+from boundary_sh import boundary_shrink
+from FaLW import FaLW
+from SFRon import SFRonExe
+
 
 def raw(data_loaders, model, criterion, args, mask=None):
     pass
